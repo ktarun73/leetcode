62232015ff892337b95c9867b6de7cf7e6e9b9a0
Tarun Kumar <ktarun7318@gmail.com>
2022-03-17 21:13:15 +0530

    Time: 4 ms (64.55%), Space: 127 MB (50.37%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -3,28 +3,29 @@ class Solution {
         int n=nums.length;
         boolean one=false;
         for(int i=0;i<n;i++){
-            if(nums[i]==1){
-                one=true;
-            }
-            if(nums[i]>n || nums[i]<1){
+            if(nums[i]<1||nums[i]>n){
                 nums[i]=1;
             }
-            
+            else if(nums[i]==1){
+                one = true;
+            }
         }
         if(one==false){
             return 1;
         }
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<n;i++){
             if(nums[Math.abs(nums[i])-1]<0){
                 continue;
             }
             nums[Math.abs(nums[i])-1]*=-1;
         }
-        for(int i=0;i<nums.length;i++){
+        
+        for(int i=0;i<n;i++){
             if(nums[i]>0){
                 return i+1;
             }
         }
+        
         return n+1;
     }
 }
