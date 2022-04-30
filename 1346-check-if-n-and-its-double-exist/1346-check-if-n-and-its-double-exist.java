6f4ab6910b529a20ce4272aeb7a9a69fde16795e
Tarun Kumar <ktarun7318@gmail.com>
2022-04-30 22:54:35 +0530

    Time: 3 ms (53.01%), Space: 44 MB (19.06%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,14 +1,26 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
+        Arrays.sort(arr);
         for(int i=0;i<arr.length;i++){
-            int d=arr[i]*2;
-            for(int j=0;j<arr.length;j++){
-                if(i==j) continue;
-                if(d==arr[j]){
-                    return true;
-                }
+            if(binarySearch(arr,arr[i]*2,i)){
+                return true;
             }
         }
         return false;
     }
+        public boolean binarySearch(int[] nums, int target, int index){
+        int start = 0, N =nums.length, end = N-1, mid=0;
+        while(start <= end){
+             mid = start + (end - start)/2;
+            if(nums[mid] == target && mid!=index) {
+                    return true;
+            }else if(target < nums[mid]){
+                end = mid-1;
+            } else{
+                start = mid+1;
+            }
+        }
+        return false;
+    }
+
 }
